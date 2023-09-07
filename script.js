3161f8b2d8ad28edc2a8a3ec342e233b5d93feb8
Enielect <eniolafaithful57@gmail.com>
2023-09-07 14:43:59 +0100

    add the script and html file

---
+++ script.js
@@ -0,0 +1,120 @@
+let more = document.querySelector('.more');
+let remaining = document.querySelector('.remaining');
+more.addEventListener('click', () => {
+    remaining.classList.toggle('hide');
+})
+
+const mails = [
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    },
+    {
+        checkbox: d,
+        favourite: d,
+        address: 'verify@x.com',
+        messageHeader: 'New login to Twitter from ChromeMobileAndroid on Android ',
+        messageBody: '- We noticed a login to your account @enielect from a new device. Was t...',
+        date: 'Aug 28'
+    }
+]
